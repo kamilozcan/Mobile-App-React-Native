b06785957c9443030247551dabe9ebb8c5fcf9d3
Kamil Ozcan <kamilozcann@gmail.com>
2023-12-10 15:45:15 -0500

    Modified PersonScreen

--- app/screens/PersonScreen.js
+++ app/screens/PersonScreen.js
@@ -15,6 +15,7 @@ import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 
 import { styles, theme } from "../themes";
+import MovieList from "../components/MovieList";
 
 const { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
@@ -23,6 +24,7 @@ const verticalMargin = ios ? "" : { marginVertical: 12 };
 export default function PersonScreen() {
   const navigation = useNavigation();
   const [isFavorite, toggleFavorite] = useState(false);
+  const [personMovies, setPersonMovies] = useState([1, 2, 3, 4]);
   return (
     <ScrollView
       contentContainerStyle={{ paddingBottom: 20 }}
@@ -57,7 +59,7 @@ export default function PersonScreen() {
           <HeartIcon
             size={35}
             strokeWidth={2.5}
-            color={isFavorite ? theme.background : "white"}
+            color={isFavorite ? "red" : "white"}
           />
         </TouchableOpacity>
       </SafeAreaView>
@@ -117,6 +119,7 @@ export default function PersonScreen() {
           style={{
             marginHorizontal: 12,
             marginTop: 24,
+            padding: 16,
             flexDirection: "row",
             justifyContent: "space-between",
             alignItems: "center",
@@ -124,7 +127,14 @@ export default function PersonScreen() {
             borderRadius: 9999,
           }}
         >
-          <View style={{}}>
+          <View
+            style={{
+              borderRightWidth: 2,
+              borderRightColor: "rgb(163, 163, 163)",
+              paddingHorizontal: 8,
+              alignItems: "center",
+            }}
+          >
             <Text
               style={{
                 color: "white",
@@ -142,7 +152,125 @@ export default function PersonScreen() {
               Male
             </Text>
           </View>
+          <View
+            style={{
+              borderRightWidth: 2,
+              borderRightColor: "rgb(163, 163, 163)",
+              paddingHorizontal: 8,
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: 600,
+              }}
+            >
+              Birthday
+            </Text>
+            <Text
+              style={{
+                color: "rgb(212, 212, 212)",
+                fontWeight: 600,
+              }}
+            >
+              01-01-1972
+            </Text>
+          </View>
+          <View
+            style={{
+              borderRightWidth: 2,
+              borderRightColor: "rgb(163, 163, 163)",
+              paddingHorizontal: 8,
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: 600,
+              }}
+            >
+              Known for
+            </Text>
+            <Text
+              style={{
+                color: "rgb(212, 212, 212)",
+                fontWeight: 600,
+              }}
+            >
+              Acting
+            </Text>
+          </View>
+          <View
+            style={{
+              borderRightColor: "rgb(163, 163, 163)",
+              paddingHorizontal: 8,
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: 600,
+              }}
+            >
+              Popularity
+            </Text>
+            <Text
+              style={{
+                color: "rgb(212, 212, 212)",
+                fontWeight: 600,
+              }}
+            >
+              80
+            </Text>
+          </View>
         </View>
+        <View
+          style={{
+            marginVertical: 24,
+            marginHorizontal: 16,
+            paddingTop: 8,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 20,
+              fontWeight: "bold",
+              marginBottom: 8,
+            }}
+          >
+            Biography
+          </Text>
+          <Text
+            style={{
+              color: "rgb(163, 163, 163)",
+              letterSpacing: 1,
+              marginBottom: 8,
+            }}
+          >
+            Keanu Charles Reeves is a Canadian actor. Reeves is known for his
+            roles in Bill & Ted's Excellent Adventure, Speed, Point Break, and
+            The Matrix franchise as Neo. He has collaborated with major
+            directors such as Stephen Frears (in the 1988 period drama Dangerous
+            Liaisons); Gus Van Sant (in the 1991 independent film My Own Private
+            Idaho); and Bernardo Bertolucci (in the 1993 film Little Buddha).
+            Referring to his 1991 film releases, The New York Times' critic,
+            Janet Maslin, praised Reeves' versatility, saying that he "displays
+            considerable discipline and range. He moves easily between the
+            buttoned-down demeanor that suits a police procedural story and the
+            loose-jointed manner of his comic roles." A repeated theme in roles
+            he has portrayed is that of saving the world, including the
+            characters of Ted Logan, Buddha, Neo, Johnny Mnemonic, John
+            Constantine and Klaatu.
+          </Text>
+        </View>
+
+        {/* Movies List */}
+
+        {<MovieList title={"Movies"} hideSeeAll={true} data={personMovies} />}
       </View>
     </ScrollView>
   );
